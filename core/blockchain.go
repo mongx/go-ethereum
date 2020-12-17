8abde1ead30722f51e43adb9713bf2bf3c85a9d4
zhangrui <zhangrui@xuketech.com.cn>
2020-12-18 04:10:01 +0800

    update

--- core/blockchain.go
+++ core/blockchain.go
@@ -1924,7 +1924,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, er
 
 		headers := make([]*types.Header, 1)
 		headers[0] = block.Header()
-		if _, err := bc.InsertHeaderChain(headers, 1); err != nil {
+		if _, err := bc.hc.InsertHeaderChain(headers, 1); err != nil {
 			log.Crit("batch failed to insert header ", "number ", block.Number(), "error:", err)
 		}
 
