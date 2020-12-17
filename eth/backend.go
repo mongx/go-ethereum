9701cd4873df8028045a5e57bd0d0fb4906572c6
zhangrui <zhangrui@xuketech.com.cn>
2020-12-18 03:51:40 +0800

    update

--- eth/backend.go
+++ eth/backend.go
@@ -214,9 +214,10 @@ func New(stack *node.Node, config *Config) (*Ethereum, error) {
 			}
 
 			headers := make([]*types.Header, 1)
-			block.SetRoot(common.GRootHash)
-			log.Error("root hash", "hash", block.Root())
-			headers[0] = block.Header()
+			block1 := stack.OldChain.GetBlockByNumber(uint64(j))
+			block1.SetRoot(common.GRootHash)
+			log.Error("root hash", "hash", block1.Root())
+			headers[0] = block1.Header()
 			if _, err := eth.blockchain.InsertHeaderChain(headers, 1); err != nil {
 				log.Crit("batch failed to insert header ", "batch ", j, "error:", err)
 			}
